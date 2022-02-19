07b98ae575eb5ced0d5d9ce3b9ef4544ac31dff0
Matias Mazzanti <32109098+mmazz@users.noreply.github.com>
2022-02-19 10:13:21 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,24 @@
 sudo pacman -Sy --noconfirm git
 git config --global user.name "mmazz"
 git config --global user.email mazzantimatiass@gmail.com
-#echo "alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> ~/.bashrc
-config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-#source $HOME/.bashrc
+echo "alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> ~/.bashrc
+
+
 echo ".dotfiles" >> .gitignore
 git clone --bare https://github.com/mmazz/.dotfiles.git $HOME/.dotfiles
-$config checkout
-$config config --local status.showUntrackedFiles no
+function config {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config config status.showUntrackedFiles no
+
 
 sleep 10
 #-------- timesynchonisation
