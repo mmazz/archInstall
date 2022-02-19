da07d8b283e86667ea15704d20d36b9160043ab1
Matias Mazzanti <32109098+mmazz@users.noreply.github.com>
2022-02-19 11:01:25 -0300

    dmenu fix

--- setup.sh
+++ setup.sh
@@ -76,7 +76,7 @@ cd st
 sudo make clean install
 
 cd ..
-git clone --depth 1 https://github.com/mmazz/dmenu.git
+git clone --depth 1 https:///git.suckless.org/dmenu
 cd dmenu
 sudo make clean install
 rm -rf .git
