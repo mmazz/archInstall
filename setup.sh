2be6b9f25c21048e776f0114e7ab631aac2ad5d5
Matias Mazzanti <32109098+mmazz@users.noreply.github.com>
2022-02-20 17:10:51 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo "alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" 
 
 
 echo ".dotfiles" >> .gitignore
-git clone --bare https://github.com/mmazz/.dotfiles.git $HOME/.dotfiles
+git clone --bare --recurse-submodules https://github.com/mmazz/.dotfiles.git $HOME/.dotfiles
 function config {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
@@ -18,6 +18,7 @@ if [ $? = 0 ]; then
     sudo config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 config checkout
+config submodule update --init --recursive
 config config status.showUntrackedFiles no
 
 
