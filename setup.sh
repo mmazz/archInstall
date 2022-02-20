14c183a10a44a63adc8f0317bccece9ef9266657
Matias Mazzanti <32109098+mmazz@users.noreply.github.com>
2022-02-20 14:16:35 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,6 +17,7 @@ if [ $? = 0 ]; then
     echo "Backing up pre-existing dot files.";
     sudo config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
+config checkout
 config config status.showUntrackedFiles no
 
 
